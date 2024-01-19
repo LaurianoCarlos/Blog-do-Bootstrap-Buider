af6749ec17dd57ef03cf1ada211d1ce6c43a1fd2
Lauriano <laurianocarlos@hotmail.com>
2024-01-19 15:47:48 -0300

    upload de arquivos

---
+++ index.php
@@ -0,0 +1,234 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog do Bootstrap Builder</title>
+
+    <link rel="icon" href="https://getbootstrap.com/docs/4.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="stylesheet" href="_cdn/css/app.css">
+</head>
+
+<body>
+    <header class="main_header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-6 main_header_logo">
+                    <h1 class="text-center mb-3 mb-lg-0 text-lg-left">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 612 612" role="img" focusable="false">
+                            <title>Bootstrap Builder</title>
+                            <path fill="currentColor" d="M510 8a94.3 94.3 0 0 1 94 94v408a94.3 94.3 0 0 1-94 94H102a94.3 94.3 0 0 1-94-94V102a94.3 94.3 0 0 1 94-94h408m0-8H102C45.9 0 0 45.9 0 102v408c0 56.1 45.9 102 102 102h408c56.1 0 102-45.9 102-102V102C612 45.9 566.1 0 510 0z"></path>
+                            <path fill="currentColor" d="M196.77 471.5V154.43h124.15c54.27 0 91 31.64 91 79.1 0 33-24.17 63.72-54.71 69.21v1.76c43.07 5.49 70.75 35.82 70.75 78 0 55.81-40 89-107.45 89zm39.55-180.4h63.28c46.8 0 72.29-18.68 72.29-53 0-31.42-21.53-48.78-60-48.78h-75.57zm78.22 145.46c47.68 0 72.73-19.34 72.73-56s-25.93-55.37-76.46-55.37h-74.49v111.4z"></path>
+                        </svg>
+                    </h1>
+                </div>
+                <div class="col-12 col-lg-6  main_header_menu ">
+                    <ul class="d-flex flex-wrap justify-content-center justify-content-lg-end align-content-stretch">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Curso</a></li>
+                        <li><a href="">Escola</a></li>
+                        <li><a href="">Contato</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main class="main_content">
+        <article class="jumbotron jumbotron-fluid">
+            <div class="container text-center text-md-left">
+                <h2 class="display-4">Bem vindo ao Bootstrap <b>Builder</b>!</h2>
+                <p class="lead">Crie projetos responsivos e móveis na Web com a biblioteca de componentes front-end mais
+                    popular do mundo..</p>
+                <hr class="my-4">
+                <p>O Bootstrap é um kit de ferramentas de código aberto para desenvolvimento com HTML, CSS e JS.</p>
+                <a class="btn btn-outline-white btn-lg" href="#" role="button">Saiba mais</a>
+            </div>
+        </article>
+
+        <div class="container my-5">
+
+            <section class="row align-items-center">
+                <article class="col-12 col-sm-6 col-lg-3  mb-3 d-flex align-self-stretch">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Rápido e Fácil</h5>
+                            <p class="card-text">A alta curva de aprendizado faz com que você entregue mais rápido.</p>
+                        </div>
+                    </div>
+                </article>
+                <article class="col-12 col-sm-6 col-lg-3 mb-3 d-flex align-self-stretch">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Responsivo</h5>
+                            <p class="card-text">Exiba o conteúdo independente de resolução ou dispositivo.</p>
+                        </div>
+                    </div>
+                </article>
+                <article class="col-12 col-sm-6 col-lg-3 mb-3 d-flex align-self-stretch">
+                    <div class="card">
+                        <div class="card-body text-center ">
+                            <h5 class="card-title">Base SASS</h5>
+                            <p class="card-text">Incorpore recursos do SASS a sua folha de estilo para ser profissional e
+                                elegante.</p>
+                        </div>
+                    </div>
+                </article>
+                <article class="col-12 col-sm-6 col-lg-3 mb-3 d-flex align-self-stretch">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Documentação Repleta</h5>
+                            <p class="card-text">Tenha acesso rápido a vários exemplos de uso de cada um dos recursos.</p>
+                        </div>
+                    </div>
+                </article>
+            </section>
+
+            <section class="row mt-5 main_last_articles">
+                <div class="col-12 mt-5 mb-4">
+                    <h2>Últimos Artigos</h2>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-4 mb-4 d-flex align-self-stretch">
+                    <div class="card">
+                        <img src="https://picsum.photos/1200/768?random=1" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Documentando aplicação com UML em três tipos de diagramas</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
+                            <p class="text-center"><a href="" class="btn btn-outline-purple d-block">Saber mais...</a></p>
+                            <p class="card-text">
+                                <small class="text-muted">Última alteração a 3 minutos</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-4 mb-4 d-flex align-self-stretch">
+                    <div class="card">
+                        <img src="https://picsum.photos/1200/768?random=2" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Crie campanha de remarketing utilizando o Google AdWords</h5>
+                            <p class="card-text">Como adicionar a sua global tag, criar lista de clientes e subir
+                                uma campanha completa na rede de display do AdWords</p>
+                            <p class="text-center"><a href="" class="btn btn-outline-purple d-block">Saber mais...</a></p>
+                            <p class="card-text">
+                                <small class="text-muted">Última alteração a 3 minutos</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-4 mb-4 d-flex align-self-stretch">
+                    <div class="card">
+                        <img src="https://picsum.photos/1200/768?random=3" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Adicione efeito de profundidade na sua página com Parallax Scroll</h5>
+                            <p class="card-text">Veja como desenvolver o efeito totalmente do zero trabalhando com jQuery
+                                para monitorar o browser do usuário</p>
+                            <p class="text-center"><a href="" class="btn btn-outline-purple d-block">Saber mais...</a></p>
+                            <p class="card-text">
+                                <small class="text-muted">Última alteração a 3 minutos</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-4 mb-4 d-flex align-self-stretch">
+                    <div class="card">
+                        <img src="https://picsum.photos/1200/768?random=4" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Ambiente WEB na Amazon AWS EC2 com Recursos Gratuitos</h5>
+                            <p class="card-text">Crie uma máquina para abrigar o wordpress com serviços
+                                necessários usando a infra gratis da AWS</p>
+                            <p class="text-center"><a href="" class="btn btn-outline-purple d-block">Saber mais...</a></p>
+                            <p class="card-text">
+                                <small class="text-muted">Última alteração a 3 minutos</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-4 mb-4 d-flex align-self-stretch">
+                    <div class="card">
+                        <img src="https://picsum.photos/1200/768?random=5" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Desenvolvendo Login Step by Step com Ajax e PHP</h5>
+                            <p class="card-text">Apresente boas vindas ao usuário antes mesmo de solicitar a senha sem
+                                precisar recarregar a página</p>
+                            <p class="text-center"><a href="" class="btn btn-outline-purple d-block">Saber mais...</a></p>
+                            <p class="card-text">
+                                <small class="text-muted">Última alteração a 3 minutos</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-4 mb-4 d-flex align-self-stretch">
+                    <div class="card">
+                        <img src="https://picsum.photos/1200/768?random=6" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Crie um formulário step by step com notificação via SMS</h5>
+                            <p class="card-text">Veja nessa aula como que você pode criar um formulário de cadastro para
+                                autenticar a conta e os dados informados</p>
+                            <p class="text-center"><a href="" class="btn btn-outline-purple d-block">Saber mais...</a></p>
+                            <p class="card-text">
+                                <small class="text-muted">Última alteração a 3 minutos</small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+
+    <div class="main_footer">
+        <section class="container">
+            <div class="row py-5">
+                <article class="col-12 col-md-6 col-lg-4">
+                    <h4>Bootstrap <b>Builder</b></h4>
+
+                    <p>Leve o desenvolvimento dos seus layouts para um próximo nível usando o Bootstrap 5. Veja como é fácil
+                        e rápido desenvolver uma aplicação de qualidade!</p>
+                </article>
+
+                <article class="col-12 col-md-6 col-lg-4">
+                    <h4>Lorem Ipsum is simply</h4>
+
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Curso</a></li>
+                        <li><a href="">Escola</a></li>
+                        <li><a href="">Contato</a></li>
+                    </ul>
+                </article>
+
+                <article class="col-12 col-lg-4  text-left  mx-auto">
+                    <h4>Redes Sociais</h4>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <p>
+                                <a href="https://www.instagram.com/lauriano.carlos"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+                                    </svg></i></a>
+
+                                <a href="https://www.facebook.com/lauriano.carlos.52/"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                                        <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+                                    </svg></a>
+
+                                <a href="https://github.com/LaurianoCarlos?tab=repositories"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                                    </svg>
+                                </a>
+                            </p>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </section>
+    </div>
+
+    <footer class="bg-purple py-3">
+        <p class="text-center text-white mb-0">Todos os Direitos Reservados ® 2024 | Lauriano Carlos</p>
+    </footer>
+
+    <script src="_cdn/js/jquery-3.4.1.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
